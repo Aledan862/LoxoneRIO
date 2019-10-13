51eb89611ce74fc357e85ca4fb441fbf26e5a0eb
Aledan862 <vakhrushev@technolink.spb.ru>
2019-10-13 22:17:24 +0300

    init

---
+++ src/primary.h
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include "options.h"
+#include "utils.h"
+#include <string.h>
+#include <Cmd.h>
+#ifndef ETHERNET_DISABLE
+#include "ethernetserver.h"
+#endifcd
+
+void main_setup();
+
+void main_loop();
+
+void setupCmdArduino();
+void cmdFunctionHelp(int arg_cnt, char **args);
